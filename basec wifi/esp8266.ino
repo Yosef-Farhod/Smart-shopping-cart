afb085159d35e6f987aedb1dbf7f23fa8f0a8007
Yosef <xuop87@gmail.com>
2025-06-22 23:45:08 +0300

    Add basec wifi

---
+++ basec wifi/esp8266.ino
@@ -0,0 +1,55 @@
+//قراءه // حساس الوزن HX711 مع ESP8266 وإرسال البيانات إلى خادم HTTP
+// هذا الكود يستخدم مكتبة HX711 لقراءة الوزن من حساس HX711 ويرسل البيانات إلى خادم HTTP عبر WiFi.
+
+
+
+#include <ESP8266WiFi.h>
+#include <HX711.h>
+#include <ESP8266HTTPClient.h>
+
+const char* ssid = "ESP32_Test";
+const char* password = "12345678";
+const char* host = "192.168.4.1";
+
+#define DT D5
+#define SCK D6
+
+HX711 scale;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected to WiFi");
+
+  scale.begin(DT, SCK);
+  scale.set_scale();
+  scale.tare();
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    float weight = scale.get_units();
+    weight = (weight < 0) ? 0 : weight;
+
+    WiFiClient client;
+    HTTPClient http;
+    String url = "http://" + String(host) + "/update?weight=" + String(weight, 2);
+    http.begin(client, url);  // ✅ تم التعديل هنا
+    int httpCode = http.GET();
+
+    if (httpCode > 0) {
+      String response = http.getString();
+      Serial.println("Server response: " + response);
+    } else {
+      Serial.println("Connection failed");
+    }
+
+    http.end();
+  }
+
+  delay(2000);
+}
