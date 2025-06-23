45e3051d46b3251efe8cade9ea92bdf977f3668f
Yosef <xuop87@gmail.com>
2025-06-23 15:15:49 +0300

    Show date priducit from firebase and trinaslat to esp32

--- main/esp32-main.ino
+++ main/esp32-main.ino
@@ -6,42 +6,42 @@ const char *password = "28072004";
 
 WebServer server(80);
 
-float latestWeight = 0.0;
+String latestSerial = ""; // متغير لحفظ الرقم التسلسلي
 
 void handleUpdate()
 {
-  Serial.println("Received request on /update");
-  if (server.hasArg("weight"))
-  {
-    latestWeight = server.arg("weight").toFloat();
-    Serial.print("Received weight: ");
-    Serial.println(latestWeight);
-    server.send(200, "text/plain", "Weight received");
-  }
-  else
-  {
-    server.send(400, "text/plain", "Missing weight");
-  }
+    Serial.println("Received request on /update");
+    if (server.hasArg("serial"))
+    {
+        latestSerial = server.arg("serial");
+        Serial.print("Received serial: ");
+        Serial.println(latestSerial);
+        server.send(200, "text/plain", "Serial received");
+    }
+    else
+    {
+        server.send(400, "text/plain", "Missing serial");
+    }
 }
 
 void setup()
 {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password); // اتصال كعميل وليس كنقطة وصول
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nConnected to WiFi");
-  Serial.print("IP Addres ESP32 : ");
-  Serial.println(WiFi.localIP());
+    Serial.begin(115200);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nConnected to WiFi");
+    Serial.print("IP Addres ESP32 : ");
+    Serial.println(WiFi.localIP());
 
-  server.on("/update", handleUpdate);
-  server.begin();
+    server.on("/update", handleUpdate);
+    server.begin();
 }
 
 void loop()
 {
-  server.handleClient();
+    server.handleClient();
 }
--- main/esp8266-main.ino
+++ main/esp8266-main.ino
@@ -2,9 +2,9 @@
 #include <ESP8266HTTPClient.h>
 #include <FirebaseESP8266.h>
 
-const char* ssid = "Yosef";
-const char* password = "28072004";
-const char* host = "192.168.43.21"; // IP ESP32
+const char *ssid = "Yosef";
+const char *password = "28072004";
+const char *host = "192.168.43.21"; // IP ESP32
 
 #define API_KEY "AIzaSyCCi6Yvyfh7zPY_DqczkMcBUFdkmmI8xTA"
 #define DATABASE_URL "https://smart-cart-f9c56-default-rtdb.firebaseio.com/"
@@ -21,71 +21,78 @@ float price = 0;
 String serial = "";
 String shelf = "";
 
-void setup() {
-  Serial.begin(115200);
+void setup()
+{
+    Serial.begin(115200);
 
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println();
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
 
-  config.api_key = API_KEY;
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
-  config.database_url = DATABASE_URL;
-  Firebase.reconnectNetwork(true);
-  fbdo.setBSSLBufferSize(1024, 1024);
-  fbdo.setResponseSize(1024);
-  Firebase.begin(&config, &auth);
-  Firebase.setDoubleDigits(5);
-  config.timeout.serverResponse = 10 * 1000;
+    config.api_key = API_KEY;
+    auth.user.email = USER_EMAIL;
+    auth.user.password = USER_PASSWORD;
+    config.database_url = DATABASE_URL;
+    Firebase.reconnectNetwork(true);
+    fbdo.setBSSLBufferSize(1024, 1024);
+    fbdo.setResponseSize(1024);
+    Firebase.begin(&config, &auth);
+    Firebase.setDoubleDigits(5);
+    config.timeout.serverResponse = 10 * 1000;
 
-  // جلب بيانات المنتج من فايربيز
-  if (Firebase.getString(fbdo, "/products/CyhYDpfJgNTcpQpMcfWK/name"))
-    name = fbdo.stringData();
-  if (Firebase.getFloat(fbdo, "/products/CyhYDpfJgNTcpQpMcfWK/price"))
-    price = fbdo.floatData();
-  if (Firebase.getString(fbdo, "/products/CyhYDpfJgNTcpQpMcfWK/serial"))
-    serial = fbdo.stringData();
-  if (Firebase.getString(fbdo, "/products/CyhYDpfJgNTcpQpMcfWK/shelf"))
-    shelf = fbdo.stringData();
+    // جلب بيانات المنتج من فايربيز
+    if (Firebase.getString(fbdo, "/products/CyhYDpfJgNTcpQpMcfWK/name"))
+        name = fbdo.stringData();
+    if (Firebase.getFloat(fbdo, "/products/CyhYDpfJgNTcpQpMcfWK/price"))
+        price = fbdo.floatData();
+    if (Firebase.getString(fbdo, "/products/CyhYDpfJgNTcpQpMcfWK/serial"))
+        serial = fbdo.stringData();
+    if (Firebase.getString(fbdo, "/products/CyhYDpfJgNTcpQpMcfWK/shelf"))
+        shelf = fbdo.stringData();
 
-  // عرض بيانات المنتج
-  Serial.println("بيانات المنتج:");
-  Serial.print("الاسم: ");
-  Serial.println(name);
-  Serial.print("السعر: ");
-  Serial.println(price);
-  Serial.print("الرقم التسلسلي: ");
-  Serial.println(serial);
-  Serial.print("الرف: ");
-  Serial.println(shelf);
-  Serial.println("----------------------");
+    // عرض بيانات المنتج
+    Serial.println("بيانات المنتج:");
+    Serial.print("الاسم: ");
+    Serial.println(name);
+    Serial.print("السعر: ");
+    Serial.println(price);
+    Serial.print("الرقم التسلسلي: ");
+    Serial.println(serial);
+    Serial.print("الرف: ");
+    Serial.println(shelf);
+    Serial.println("----------------------");
 }
 
-void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    // إرسال الرقم التسلسلي فقط
-    WiFiClient client;
-    HTTPClient http;
-    String url = "http://" + String(host) + "/update?serial=" + serial;
-    http.begin(client, url);
-    int httpCode = http.GET();
+void loop()
+{
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        // إرسال الرقم التسلسلي فقط
+        WiFiClient client;
+        HTTPClient http;
+        String url = "http://" + String(host) + "/update?serial=" + serial;
+        http.begin(client, url);
+        int httpCode = http.GET();
 
-    if (httpCode > 0) {
-      String response = http.getString();
-      Serial.println("Server response: " + response);
-    } else {
-      Serial.println("Connection failed");
+        if (httpCode > 0)
+        {
+            String response = http.getString();
+            Serial.println("Server response: " + response);
+        }
+        else
+        {
+            Serial.println("Connection failed");
+        }
+
+        http.end();
     }
 
-    http.end();
-  }
-
-  delay(2000);
+    delay(2000);
 }
